0745d0176cd2d7a0cfb892c70990cc26e2ac100a
16 Caracteres <camille@16caracteres.com>
2024-07-04 17:33:28 +0200

    Démarrage étape 3, intégration et appel référence dans la modale

--- js/script.js
+++ js/script.js
@@ -2,12 +2,21 @@ console.log("on me récupère bien");
 
 const modale = document.getElementById('modale-contact');
 const boutonContact = document.getElementById('menu-item-20');
+const boutonContactSite = document.querySelector('.btn__contact');
 const boutonBurger = document.querySelector('.menu__burger');
 const menu = document.querySelector('.menu__list');
+// Ajout Ref Input
+const inputRef = document.getElementById('reference');
+const reference = document.querySelector('.photo__reference');
 
 // Modale Contact
 boutonContact.addEventListener("click", () => {
     modale.classList.add("active");
+})
+
+boutonContactSite.addEventListener("click", () => {
+    modale.classList.add("active");
+    inputRef.value = reference.innerHTML;
 })
 
 modale.addEventListener("click", (event) => {
@@ -21,3 +30,6 @@ modale.addEventListener("click", (event) => {
 boutonBurger.addEventListener("click", () => {
     menu.classList.toggle("active");
 })
+
+console.log(inputRef);
+console.log(reference);
--- single-photos.php
+++ single-photos.php
@@ -1,16 +1,44 @@
 <?php get_header(); ?>
 
-<main class="photos__list">
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    	<div class="photo">
-        	<h2 class="photo__title">
-                <a href="<?php the_permalink(); ?>">
-                	<?php the_title(); ?>
-                </a>
-            </h2>
-         	<?php the_post_thumbnail(); ?>
-    	</div>
-    <?php endwhile; endif; ?>
+<main class="photo__list">
+    <section class="photo__container">
+        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+            <div class="photo__data">
+                <h2 class="photo__title">
+                    <?php the_title(); ?>
+                </h2>
+                <ul class="photo__dataList">
+                    <li>Référence : 
+                        <span class="photo__reference"><?php the_field( 'reference' ); ?></span>
+                    </li>
+                    <li>Catégorie : <?php the_terms( get_the_ID() , 'categorie-photos' ); ?></li>
+                    <li>Format : <?php the_terms( get_the_ID() , 'format-photos' ); ?></li>
+                    <li>Type : <?php the_field( 'type_de_photo' ); ?></li>
+                    <li>Année : <?php the_time('Y'); ?></li>
+                </ul>                 
+            </div>
+            <div class="photo__thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div>
+        <?php endwhile; endif; ?>
+
+        <div class="photo__contact">
+            <p>Cette photo vous intéresse ?</p>
+            <button class="btn__contact">Contact</button>
+        </div>
+        <div class="photo__pagination">
+            <div class="photo__preview"></div>
+            <div class="pagination__arrows">
+                <img src="" alt="" class="arrow_left">
+                <img src="" alt="" class="arrow_right">
+            </div>
+        </div>
+        
+    </section>
+	
+
+
+    <!-- Template part Vous aimerez aussi -->
 </main> 
 
 <?php get_footer(); ?>
