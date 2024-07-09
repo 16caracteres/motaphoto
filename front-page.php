5eea02b6d7992b0edf404e457971204bccb7416e
16 Caracteres <camille@16caracteres.com>
2024-07-09 18:39:19 +0200

    Etape 4 Boucle Requête Liste de photos

--- front-page.php
+++ front-page.php
@@ -5,23 +5,45 @@
         <?php get_template_part( 'parts/photos_hero' ); ?>
         <h1 class="hero__title">Photographe Event</h1>
     </section>
-    <section class="photo__list">
+    <section class="photos">
         <div class="photos__filters">
-
+            <form action="">
+                <select name="" id=""> 
+                    <option value="Catégories">Catégories</option>
+                    <option value=""></option> 
+                </select>
+                <select name="" id="">
+                    <option value="Formats">Formats</option>
+                    <option value=""></option> 
+                </select>
+                <select name="" id="">
+                    <option value="Trier par">Trier par</option>
+                    <option value=""></option> 
+                </select>
+            </form>
         </div>
-        
-        <?php 
-        // Définir les arguments personnalisés
-            $args = array(
-                'post_type' => 'photos',
-                'posts_per_page' => 8, // Modifier le nombre de photos à afficher
-                'orderby' => 'date' // Par exemple, trier par date au lieu de aléatoire
-                
-            );
 
-            // Inclure le template-part avec les arguments
-            get_template_part('template-photo-list', null, array('args' => $args));
-        ?>
+        <div class="photo__list">
+            <?php 
+                $args = array(
+                    'post_type' => 'photos',
+                    'posts_per_page' => 8,
+                    'order' => 'date'
+                );
+
+                $homepage_query = new WP_Query( $args );
+
+                if( $homepage_query->have_posts() ) : while( $homepage_query->have_posts() ) : $homepage_query->the_post();
+                    
+                    get_template_part( 'parts/photos_list' ); 
+
+                    endwhile;
+                    endif;
+
+                wp_reset_postdata();
+            ?>
+        
+        </div>
 
     </section>
 </main>
--- single-photos.php
+++ single-photos.php
@@ -55,7 +55,51 @@
     <!-- Template part Vous aimerez aussi -->
     <section class="photos__suggestion">
         <h4>Vous aimerez aussi</h4>
-        <?php get_template_part( 'parts/photos_list' ); ?>
+        <div class="photo__list">
+            <?php
+                //Récupérer la catégorie de photo en cours
+                $categories = get_the_terms(get_the_ID(), 'categorie-photos');
+                $current_category_slugs = array();
+
+                if ($categories) {
+                    foreach ($categories as $category) {
+                        $current_category_slugs[] = $category->slug;
+                    }
+                }
+                
+                // On définit les arguments pour définir ce que l'on souhaite récupérer
+                $args = array(
+                    'post_type' => 'photos',
+                    'posts_per_page' => 2,
+                    'orderby' => 'rand',
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => 'categorie-photos',
+                            'field' => 'slug',
+                            'terms' => $current_category_slugs
+                        ),
+                        ),
+                    'post__not_in' => [get_the_ID()]
+                );
+
+                // On exécute la WP Query
+                $suggestion_query = new WP_Query( $args );
+            
+                // On lance la boucle !
+                if( $suggestion_query->have_posts() ) : while( $suggestion_query->have_posts() ) : $suggestion_query->the_post();
+                    
+                    
+                    get_template_part( 'parts/photos_list' ); 
+            
+                    
+                endwhile;
+                endif;
+            
+                // 4. On réinitialise à la requête principale (important)
+                wp_reset_postdata();               
+
+            ?>
+        </div>
     </section>
     
 
